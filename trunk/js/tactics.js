151e67feea5c0121b287b8f988464ed3fc6137c9
freeon.ne@gmail.com <freeon.ne@gmail.com@9aa15318-5452-2bd2-1613-523dc2abd19c>
2012-12-15 22:15:45 +0000

    Создание игроков через код, заодно курсор исправил

--- trunk/js/tactics.js
+++ trunk/js/tactics.js
@@ -1,12 +1,33 @@
 (function ($) {
 
+    var playerDragOptions = {
+        containment: "parent",
+        cursor: "pointer",
+        grid: [24, 24],
+        scroll: false
+    };
+
     var init = function() {
-        $('.tactics-field-player').draggable({
-            containment: "parent",
-            cursor: "pointer",
-            grid: [24, 24],
-            scroll: false
+        $('.tactics-field-player').draggable(playerDragOptions);
+
+        createPlayer(240, 160, 23, 'Аршавин');
+        createPlayer(58, 60, 8, 'Самир Насри');
+        createPlayer(58, 160, 29, 'Халк');
+        createPlayer(58, 260, 9, 'Роналдо Луис Назарио де Лима');
+    };
+
+    var createPlayer = function(x, y, number, name) {
+
+        var playerDiv = $('<div class="tactics-field-player"></div>').appendTo($('.tactics-field'));
+        playerDiv.append('<div class="tactics-field-player-number">' + number +  '</div>');
+        playerDiv.append('<div class="tactics-field-player-name">' + name + '</div>');
+        playerDiv.css({
+            'position': 'absolute',
+            'left':x,
+            'top': y
         });
+
+        playerDiv.draggable(playerDragOptions);
     };
 
     $(document).ready(init);
